b40f506338bb02c1421fb827b8eff9272e8d4481
Houssem Hammami <hammamihossem99@gmail.com>
2022-03-18 15:58:21 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [myclass, setMyClass] = useState("");
+  const [fac, setFac] = useState("");
+
+  const handelchangeNom = (event) => {
+    setNom(event.target.value);
+  };
+
+  const handelchangePrenom = (event) => {
+    setPrenom(event.target.value);
+  };
+
+  const handelchangeClass = (event) => {
+    setMyClass(event.target.value);
+  };
+
+  const handelchangeFac = (event) => {
+    setFac(event.target.value);
+  };
+
+  const affiche = () => {
+    alert(`nom:${nom} prenom:${prenom} class:${myclass} fac:${fac}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <fieldset>
+        <legend>Information personnel</legend>
+        <table>
+          <tr>
+            <td>
+              <label>Nom</label>
+              <input type="text" onChange={handelchangeNom}></input>
+            </td>
+            <td>
+              <label for="prenom">Prenom</label>
+              <input
+                id="prenom"
+                type="text"
+                onChange={handelchangePrenom}
+              ></input>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label>class</label>
+              <input type="text" onChange={handelchangeClass}></input>
+            </td>
+            <td>
+              <label>fac</label>
+              <select onChange={handelchangeFac}>
+                <option selected></option>
+                <option value="fst">fst</option>
+                <option value="fst">iset nabeul</option>
+                <option value="fst">fss</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button onClick={affiche}>Affiche</button>
+            </td>
+          </tr>
+        </table>
+      </fieldset>
+    </>
   );
 }
 
